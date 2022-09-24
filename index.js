58ce4fae24f3285e643f059c07901c49a852a5d9
Dave Greber <davegreber@gmail.com>
2022-09-24 18:23:36 -0400

    commit tree 8a38b2ef618e71691d292755013982a1bbad3234

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+
+// const maine = document.getElementById('main')
+// console.log(maine)
+
+// document.maine.remove(main)
+
+
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Dave is the champion"
+
+document.body.append(newHeader)
